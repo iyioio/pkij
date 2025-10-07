36e7e76be415e3c312f1d9385a0b94341813d68b
Scott Vance <scott@iyio.io>
2025-10-07 08:52:49 -0400

    - install script note

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This scripts installs pkij into /user/local/bin by creating a symlink from the local ./pkij.js file
+# to /usr/local/bin/pkij. This will allow you to run the pkij command anywhere on your computer
+# and updates to pkij.js will be instantly seen, no build required.
+
 set -e
 cd "$(dirname "$0")"
 
